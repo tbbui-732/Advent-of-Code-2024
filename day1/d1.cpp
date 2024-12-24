1eabf9e8f406731359f8b712c12dba850ca5388e
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2024-12-23 23:03:47 -0500

    Solved day 1

--- day1/d1.cpp
+++ day1/d1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <unordered_map>
 
 void part1() {
     std::fstream file("d1.txt");
@@ -34,7 +35,34 @@ void part1() {
 }
 
 void part2() {
+    std::fstream file("d1.txt");
+    std::vector<int> left, right;
+    left.reserve(1000), right.reserve(1000);
 
+    // read file input
+    if (file.is_open()) {
+        std::string line;
+        while (std::getline(file, line)) {
+            left.push_back(stoi(line.substr(0, 5)));
+            right.push_back(stoi(line.substr(8, 13)));
+        }
+        file.close();
+    } else {
+        std::cout << "failed to open file" << std::endl;
+        exit(-1);
+    }
+
+    // store right list values into value:count pair
+    std::unordered_map<int, int> umap;
+    for (int i = 0; i < 1000; i++) {
+        umap[right[i]]++;
+    }
+
+    int similarity = 0;
+    for (int i = 0; i < 1000; i++) {
+        similarity += umap[left[i]] * left[i];
+    }
+    std::cout << "similarity: " << similarity << std::endl;
 }
 
 int main() {
